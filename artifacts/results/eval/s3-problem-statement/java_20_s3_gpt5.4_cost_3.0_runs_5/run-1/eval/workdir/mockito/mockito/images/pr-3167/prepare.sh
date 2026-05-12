80be74fce737d6d9c1f452131181ec5ac9bddee6
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-05-12 18:38:45 +0200

    feat: save results s3-problem-statement/java_20_s3_gpt5.4_cost_3.0_runs_5

---
+++ artifacts/results/eval/s3-problem-statement/java_20_s3_gpt5.4_cost_3.0_runs_5/run-1/eval/workdir/mockito/mockito/images/pr-3167/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+cd /home/mockito
+git reset --hard
+bash /home/check_git_changes.sh
+git checkout b6554b29ed6c204a0dd4b8a670877fe0ba2e808b
+
+# apply metamorphic patch (if present)
+
+
+bash /home/check_git_changes.sh
+
+./gradlew build || true
+
